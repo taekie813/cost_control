5f08ba5cd78c9fd36e6080fc34917c8af1f9fc66
taekie <euntaek0813@naver.com>
2024-02-15 22:07:38 +0900

    update : User dto basic

--- server/src/main/java/sideProject/costControl/user/dto/UserDto.java
+++ server/src/main/java/sideProject/costControl/user/dto/UserDto.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import sideProject.costControl.user.entity.User;
 
 public class UserDto {
     @Getter
+    @Setter
+    @AllArgsConstructor
     @NoArgsConstructor
     public static class Post {
         private Long userId;
@@ -17,6 +20,8 @@ public class UserDto {
     }
 
     @Getter
+    @Setter
+    @AllArgsConstructor
     @NoArgsConstructor
     public static class Patch {
         private Long userId;
@@ -30,6 +35,8 @@ public class UserDto {
     }
 
     @Getter
+    @Setter
+    @AllArgsConstructor
     @NoArgsConstructor
     public static class PasswordPatch {
         private Long userId;
@@ -40,10 +47,16 @@ public class UserDto {
 
     @Getter
     @Setter
+    @AllArgsConstructor
     @NoArgsConstructor
     public static class Response {
         private String name;
         private String nickName;
         private String email;
+        private User.UserStatus userStatus;
+
+        public String getUserStatus() {
+            return userStatus.getStatus();
+        }
     }
 }
